cb1ce8adc0af01f1e1be1af5636e9fb8fa13f251
mesgnawoldeab <96284818+Mesgna616@users.noreply.github.com>
2022-05-24 21:14:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
 */
 var images;
 
-
 images = ['https://flxt.tmsimg.com/assets/p18930951_b_v13_ac.jpg', 'https://www.wikihow.com/images/thumb/0/01/Draw-Snoopy-Step-27.jpg/aid1087964-v4-1200px-Draw-Snoopy-Step-27.jpg', 'https://i.inews.co.uk/content/uploads/2020/05/snoopy.jpg', 'https://www.denofgeek.com/wp-content/uploads/2015/12/peanuts_movie_snoopy_flying_woodstock-1.jpg'];
 let element_image = document.getElementById('image');
 element_image.setAttribute("src", images[0]);
