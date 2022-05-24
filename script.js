3e347a2c975489df6de27fe4bac3faba206616b1
mesgnawoldeab <96284818+Mesgna616@users.noreply.github.com>
2022-05-24 20:43:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 var images;
 
 
-images = ['https://flxt.tmsimg.com/assets/p18930951_b_v13_ac.jpg', 'https://www.wikihow.com/images/thumb/0/01/Draw-Snoopy-Step-27.jpg/aid1087964-v4-1200px-Draw-Snoopy-Step-27.jpg', 'https:///wp-content/uploads/2021/11/Snoopy_In_Space_Photo_020201-1014x570.jpg', 'https://www.denofgeek.com/wp-content/uploads/2015/12/peanuts_movie_snoopy_flying_woodstock-1.jpg'];
+images = ['https://flxt.tmsimg.com/assets/p18930951_b_v13_ac.jpg', 'https://www.wikihow.com/images/thumb/0/01/Draw-Snoopy-Step-27.jpg/aid1087964-v4-1200px-Draw-Snoopy-Step-27.jpg', 'https://i.inews.co.uk/content/uploads/2020/05/snoopy.jpg', 'https://www.denofgeek.com/wp-content/uploads/2015/12/peanuts_movie_snoopy_flying_woodstock-1.jpg'];
 let element_image = document.getElementById('image');
 element_image.setAttribute("src", images[0]);
 
@@ -27,7 +27,6 @@ document.getElementById('previous').addEventListener('click', (event) => {
   element_image3.setAttribute("src", images[0]);
 
 });
-
 const {
   core: { test, expect, run },
   prettify
